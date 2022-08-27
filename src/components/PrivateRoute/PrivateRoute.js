4cc3f93f3c997b5a8f03de64a41d2777f4026997
sadik182 <sadikurrahman728@gmail.com>
2022-08-27 23:07:28 +0600

    private route done

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import { Navigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/firebase.initialize';
 
 
-const PrivateRoute = ({children, ...rest}) => {
-    const {user} = useAuth();
-    return (
-        <Route
-        {...rest}
-        render = {({location}) => user.email? children : <Navigate
-        to={{
-            pathname: "/login",
-            state: { from: location }
-          }}
+const PrivateRoute = ({children}) => {
+    const [user] = useAuthState(auth)
+    const location = useLocation();
+    if(!user) {
+        return <Navigate to="/login" state={ {from: location}} replace />;
+    }
+    return children;
+    
+}  
+    export default PrivateRoute;
+
+
+// const PrivateRoute = ({children, ...rest}) => {
+//     const {user} = useAuth();
+//     return (
+//         <Route
+//         {...rest}
+//         render = {({location}) => user.email? children : <Navigate
+//         to={{
+//             pathname: "/login",
+//             state: { from: location }
+//           }}
         
-        ></Navigate>}
+//         ></Navigate>}
         
-        >
+//         >
             
-        </Route>
-    );
-};
-
-export default PrivateRoute;
+//         </Route>
+//     );
+// };
